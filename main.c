30ced57cf689de541280d49f734d96af30b1f846
lazarescu1 <lazarescu.sebi1@gmail.com>
2025-05-11 15:11:51 +0300

    AD conversion

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 /* Tema 2 - Procesoare Numerice de Semnal */
-/* ComandÄƒ servomotoare */
+/* Comandã servomotoare */
 #if defined(__dsPIC33F__)
+#include <stdio.h>
 #include "p33Fxxxx.h"
 #elif defined(__dsPIC24H__)
 #include "p24Hxxxx.h"
@@ -30,8 +31,8 @@ initPLL(void) {
     while(OSCCONbits.LOCK!=1) {};
 }
 
-/* FuncÈ›ional */
-/* Setare timer 3 sÄƒ facÄƒ achiziÈ›ii AD la fiecare 0.1s */
+/* Functional */
+/* Setare timer 3 sã facã achizitii AD la fiecare 0.1s */
 void
 init_Timer3(void) {
     T3CON = 0;
@@ -67,16 +68,57 @@ init_PWM1(void) {
     P1TCONbits.PTEN = 1;
 }
 
-void
-__attribute__ ((interrupt, no_auto_psv)) _T3Interrupt(void) {
-    _RB14 = ~_RB14;
-    _T3IF = 0;
+void 
+initAdc1(void) {
+ 	AD1CON1bits.AD12B = 1; // conversie AD pe 12 biti
+ 	AD1CON1bits.FORM = 0; // rezultat conversie integer
+ 	AD1CON1bits.SSRC = 2; // timerul 3 starteaza conversia
+ 	AD1CON1bits.ASAM = 1; // incepe esantionarea noii valori imediat dupa terminarea
+ 	// unei conversii
+ 	AD1CON2bits.CSCNA = 1; // scaneaza intrarile pe CH0+ in timpul achizitiei A
+ 	AD1CON2bits.CHPS = 0; // converteste doar CH0
+ 	AD1CON2bits.SMPI = 0; // incrementeaza adresa DMA dupa terminarea fiecarei conversii
+ 	AD1CON3bits.ADRC = 0; // foloseste ceasul magistralei
+ 	AD1CON3bits.ADCS = 63; // Timpul necesar unei conversii este de 22.4 us
+ 	// Ceasul pentru conversia AD are formula Tad=Tcy*(adcs+1)
+ 	// Tad=Tcy*(adcs+1)=(1/40)*64=1.6us
+	// Se seteaza intrarile analogice
+ 	AD1CSSLbits.CSS4 = 1; // Selectam intrarea analogica AN4(RB2) pentru a fi scanata
+	// Scriem registrul de configurare al portului
+	// Se va folosi doar registrul low al portului de configurare deoarece dsPIC33fj128MC802
+	// nu are implementati mai mult de 6 pini pentru ADC
+ 	AD1PCFGL=0xFFFF; // Setam toti pinii portului ADC1 pe modul digital,
+ 	// si activeaza citirea la intrarea portului
+ 	AD1PCFGLbits.PCFG4 = 0;// Setam pinul AN4(RB2) pe intrare analogica,
+	// ADC verifica voltajele pe acel pin (achizitie AD)
+ 	IFS0bits.AD1IF = 0; // Reseteaza flag-ul intreruperii convertorului AD
+ 	IPC3bits.AD1IP = 6; // Seteaza prioritatea intreruperii convertorului AD
+ 	IEC0bits.AD1IE = 1; // Permite intreruperea convertorului AD
+ 	AD1CON1bits.ADON = 1;
 }
 
-/* FuncÈ›ia care implementeaza logica apÄƒsÄƒrii butonului S2 */
+/* void
+__attribute__ ((interrupt, no_auto_psv)) _T3Interrupt(void) {
+    _RB15 = ~_RB15;
+	_RB14 = ~_RB14;
+	_RB13 = ~_RB13;
+	_RB12 = ~_RB12;
+    _T3IF = 0;
+} */
+
+/* Func?ia care implementeaza logica apãsãrii butonului S2 */
 void
 __attribute__ ((interrupt, no_auto_psv)) _INT0Interrupt (void) {
     _INT0IF = 0;
+}
+
+int ain4Buff[SAMP_BUFF_SIZE];
+int sampleCounter=0;
+
+void 
+__attribute__((interrupt, no_auto_psv)) _ADC1Interrupt(void) {
+ 	
+ 	IFS0bits.AD1IF = 0; // Achita intreruperea convertorului AD
 }
 
 /* Main function */
@@ -85,15 +127,16 @@ main(int argc, char **argv)
 {
     TRISB = 0x0000;
     _TRISB7 = 1;
+	_TRISB2 = 1; /* setare pin RB2 ca IN pt. achizitia A/D */
     PORTB = 0xF000;
     //initPLL();
-    //init_Timer3();
-    init_PWM1(); /* FuncÈ›ioneazÄƒ */
+    init_Timer3();
+    init_PWM1(); /* Functioneazã */
 
     _INT0IF = 0;
     _INT0IE = 1;
     _INT0EP = 1;
-
+	
     while(1) {}
     return 0;
 }
